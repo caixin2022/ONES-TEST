e6c276319fe7bd7bd03af4058e5debc4b73aa796
caixin <caixin@ones.cn>
2023-09-27 19:28:52 +0800

    14-15

--- learn1.go
+++ learn1.go
@@ -6,7 +6,7 @@ func main() {
 	fmt.Println("hello modify by web, modify by mac again 0815!!")
 
 	sum := 0
-	for i := 0; i < 14; i++ {
+	for i := 0; i < 15; i++ {
 		sum += i
 	}
 	fmt.Println("%d", sum)
